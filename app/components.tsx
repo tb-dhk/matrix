34102eaf9e8f5edca9b2c3ea336e91c70fee61f8
tb-dhk <joshuasoh2008@gmail.com>
2025-06-25 13:05:00 +0800

    .

--- app/components.tsx
+++ app/components.tsx
@@ -190,7 +190,8 @@ export function MetaTags(props) {
       <meta name="description" content={props.description} />
       <meta property="og:title" content={props.title} />
       <meta property="og:description" content={props.description} />
-      <meta property="og:image" content="/large.png" />
+      <meta property="og:image" content="/large.gif" />
+      <meta property="og:image:type" content="image/gif" />
       <meta property="og:image:alt" content="matrix" />
       <meta property="og:image:width" content="1824" />
       <meta property="og:image:height" content="1004" />

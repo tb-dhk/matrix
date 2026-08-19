7fd1a340ab4a7e44bb7c0cd28234fe6eb693e32c
tb-dhk <joshuasoh2008@gmail.com>
2026-08-20 00:01:46 +0800

    added support for table cell merging and wikilinks

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), nodePolyfills({
+      include: ['assert'],
+    }),
+  ],
 });
